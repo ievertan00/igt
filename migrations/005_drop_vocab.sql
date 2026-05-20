091e24e48c6113b41ef09c47fda6414b3fa3474d
ievertan00 <ievertan00@gmail.com>
2026-05-20 17:05:59 +0800

    chore: remove unused files

--- migrations/005_drop_vocab.sql
+++
@@ -1,3 +0,0 @@
--- Drop the vocab table. Vocabulary is stored exclusively in the Markdown vault
--- (IGT Vocabulary.md). SRS cards for vocab words live in srs_cards (source_type='vocab').
-DROP TABLE IF EXISTS vocab;
